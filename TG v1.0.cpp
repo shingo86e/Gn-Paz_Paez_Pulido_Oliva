9bc41799888ef1bf391d9be3a9a1fffa441d57ee
shingo86e <exequielsony@gmail.com>
2021-11-18 17:32:29 -0300

    Agregado logo de UTN en menu incio y prototipo de ingreso de usuario y contraseña en menu admin

--- TG v1.0.cpp
+++ TG v1.0.cpp
@@ -124,22 +124,58 @@ main()
 {
 	//seleccion de menu de usuarios
 	int usuariosMenu;
-	char usuarioPrueba[10];
+	char usuarioPrueba[10], contraseniaPrueba[10];
+	bool b=false;
+	
+	//::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+	system("color 1A");	
+	
 	do
 	{
-		gotoxy(7,0);
-		printf("Seleccione tipo de Modulo");
+		gotoxy(60,1);
+		printf("* *   * *   * *");
+		gotoxy(60,2);
+		printf("* *   * *   * *");
+		gotoxy(60,3);
+		printf("  * * * * * *");
+		gotoxy(60,4);
+		printf("* * * * * * * *");
+		gotoxy(60,5);
+		printf("* * * * * * * *");
+		gotoxy(60,6);
+		printf("  * * * * * *");
+		gotoxy(60,7);
+		printf("* *   * *   * *");
+		gotoxy(60,8);
+		printf("* *   * *   * *");
+		gotoxy(0,0);
+		printf("========================================");//0
+		gotoxy(11,1);
+		printf("CENTRO DE ESTETICA");//1
+		printf("\n========================================");//2
+		gotoxy(7,3);
+		printf("Seleccione tipo de Modulo");//3
+		printf("\n========================================");//4
+		printf("\n 1- Modulo Espacios");//5
+		printf("\n 2- Modulo Recepcion");//6
+		printf("\n 3- Modulo Administracion");//7
+		printf("\n Salir del sistema 0");//8
+		printf("\n Opcion: ");//9
+		gotoxy(0,9);
 		printf("\n========================================");
-		printf("\n 1- Modulo Espacios");
-		printf("\n 2- Modulo Recepcion");
-		printf("\n 3- Modulo Administracion");
-		printf("\n Salir del sistema 0");
-		printf("\n Opcion: ");
-		gotoxy(0,6);
-		printf("\n========================================");
-		gotoxy(9,6);
+		gotoxy(9,9);
 		scanf("%d",&usuariosMenu);
 		system("cls");
+		
+		/** *   * *   * *
+		* *   * *   * *
+		  * * * * * *
+		* * * * * * * *
+		* * * * * * * *
+		  * * * * * *
+		* *   * *   * *
+		* *   * *   * *  */
+		
 		switch(usuariosMenu)
 		{
 			case 1:
@@ -165,9 +201,24 @@ main()
 			case 3:
 				{
 					//Modulo Administracion
-					printf("\nIngrese Nombre de Usuario: ");
-					_flushall();
-					gets(usuarioPrueba);
+					do
+					{
+						gotoxy(1,0);
+						printf("Modulo Administracion");
+						printf("\n==========================");
+						printf("\nIngrese Nombre de Usuario: ");
+						printf("\nIngrese Contrasenia: ");
+						printf("\n==========================");
+						_flushall();
+						gotoxy(27,2);
+						gets(usuarioPrueba);
+						gotoxy(21,3);
+						gets(contraseniaPrueba);
+						b=true;
+												
+					}
+					while(b==false);
+					
 					system("cls");
 					menuAdministracion(usuarioPrueba);
 					break;
